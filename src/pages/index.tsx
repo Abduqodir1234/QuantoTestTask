ec7bf38f53c260aa5b7bb753038bf8db4c0e150c
Abduqodir1234 <gofurboyevabduqodir@gmail.com>
2022-03-11 11:39:48 +0500

    A

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
-import { Box, Container, Grid, GridItem } from "@chakra-ui/react"
+import { Box, Container, Grid, GridItem,Image } from "@chakra-ui/react"
 import { lazy, useEffect, useState } from "react"
 import Loading from "../components/Loading"
 import Menu from "../components/MobileMenu/menu"
 import useSearchPart1 from "../reactQueryHooks/useSearchPart1"
 import useSearchPart2 from "../reactQueryHooks/useSearchPart2"
+import airplain from '../public/plain.svg'
 const SearchPart1 = lazy(() => import('../components/Search/Part1'))
 const SearchPart2 = lazy(() => import('../components/Search/Part2'))
 
 let IndexPage = () => {
     let [filter,setfilter] = useState([''])
-    let [mainData,setmaindata] = useState([])
+    let [mainData,setmaindata] = useState({data:[],stop:false})
     const part1:any = useSearchPart1()
     const searchId = part1?.data?.data?.searchId
     const part2:any = useSearchPart2(searchId,{enabled:!!searchId,refetchOnWindowFocus: false,})
+    useEffect(()=>{
+        if(!part2?.data?.data?.stop){
+            part2.refetch()
+        }
+    },[part2])
     useEffect(()=>{
         let maindata=[]
         const array = filter.filter(one=>one!=='' && one !== "cheap" && one !== "fast")
@@ -44,24 +50,26 @@ let IndexPage = () => {
                 return a.price-b.price
             })
         }
-        setmaindata(maindata?.slice(0,5))
+        setmaindata({data:maindata?.slice(0,5),stop:part2?.data?.data?.stop})
     },[filter,part2?.data?.data])
-    if(searchId && part2?.data){
+    if(searchId && part2?.data && part2?.data?.data?.stop){
         return (
             <>
             <Menu  filter={filter} setfilter={(e:any)=>setfilter(e)}/>
-            <Box bg='cyan.50' py='40px' minHeight='100vh'>
+            <Box bg='cyan.50' minHeight='100vh'>
                 <Container maxW='container.xl'>
                     <Grid
-                        marginTop=""
                         templateColumns='repeat(5, 1fr)'
                         gap={4}
                     >
+                        <GridItem padding="30px" display='flex' justifyContent='center' width='100%' colSpan={5}>
+                                <Image  width="50px" height='50px' src={airplain} />
+                        </GridItem>
                         <GridItem display={{base:'none',md:'none',xl:'block',lg:'block'}} rowSpan={2} colSpan={1} >
                             <SearchPart1 filter={filter} setfilter={(e:any)=>setfilter(e)} />
                         </GridItem>
                         <GridItem colSpan={{base:5,md:5,lg:4,xl:4}} >
-                            <SearchPart2 filter={filter} setfilter={(e:any)=>setfilter(e)} data={mainData} />
+                            <SearchPart2 filter={filter} setfilter={(e:any)=>setfilter(e)} data={mainData?.data} />
                         </GridItem>
                     </Grid>
                 </Container>
